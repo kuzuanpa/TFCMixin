1e84993fdee430aec49894c3daae97a25f056560
kuzuanpa <misaka16384@outlook.com>
2023-07-12 20:12:08 +0800

    Initial commit

---
+++ src/main/java/cn/kuzuanpa/thinkerMixin/thinkerMixin.java
@@ -0,0 +1,8 @@
+package cn.kuzuanpa.thinkerMixin;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = thinkerMixin.MODID, useMetadata = true)
+public class thinkerMixin {
+    public static final String MODID = "thinkermixin";
+}
