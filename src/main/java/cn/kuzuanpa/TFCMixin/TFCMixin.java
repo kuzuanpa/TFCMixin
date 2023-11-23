2d657db33a79976bc321c6d1fb8222a2dac7650d
kuzuanpa <misaka16384@outlook.com>
2023-11-24 04:40:50 +0800

    Now It becomes TFCMixin to spawn players in TFC Dim by default

---
+++ src/main/java/cn/kuzuanpa/TFCMixin/TFCMixin.java
@@ -0,0 +1,9 @@
+package cn.kuzuanpa.TFCMixin;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = TFCMixin.MODID, useMetadata = true)
+public class TFCMixin {
+    public static final String MODID = "tfc-mixin";
+
+}
---
+++ src/main/java/cn/kuzuanpa/TFCMixin/mixin/MixinPlayerFirstJoin.java
@@ -0,0 +1,48 @@
+package cn.kuzuanpa.TFCMixin.mixin;
+
+import cpw.mods.fml.common.FMLLog;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.world.World;
+import net.minecraft.server.management.ServerConfigurationManager;
+import net.minecraft.world.WorldServer;
+import org.apache.logging.log4j.Level;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static com.bioxx.tfc.TerraFirmaCraft.TFCDimID;
+
+@Mixin(ServerConfigurationManager.class)
+public class MixinPlayerFirstJoin{
+    @Final
+    @Shadow
+    private MinecraftServer mcServer;
+
+
+    @Inject(method = "initializeConnectionToPlayer", at = @At(value = "HEAD"),remap = false)
+    public void initializeConnectionToPlayer(NetworkManager p_72355_1_, EntityPlayerMP p_72355_2_, NetHandlerPlayServer nethandlerplayserver, CallbackInfo ci){
+        World playerWorld = this.mcServer.worldServerForDimension(p_72355_2_.dimension);
+        if (this.readPlayerDataFromFile(p_72355_2_)==null)
+        {
+            p_72355_2_.dimension=TFCDimID;
+            playerWorld=this.mcServer.worldServerForDimension(TFCDimID);
+            ChunkCoordinates spawnPoint = playerWorld.provider.getRandomizedSpawnPoint();
+            p_72355_2_.setPosition(spawnPoint.posX, spawnPoint.posY, spawnPoint.posZ);
+        }
+        p_72355_2_.setWorld(playerWorld);
+        p_72355_2_.theItemInWorldManager.setWorld((WorldServer)p_72355_2_.worldObj);
+    }
+
+    @Shadow
+    public NBTTagCompound readPlayerDataFromFile(EntityPlayerMP p_72380_1_){
+        return null;
+    }
+
+}
